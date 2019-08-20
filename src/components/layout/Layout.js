10d11abd5cb07bf366477cb877c9505bfbe497c6
nicokurogane <fyedream@gmail.com>
2019-08-20 09:25:43 -0600

    Set up proect with needed dependencies

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Layout = () => {
+  return (
+    <div className="layout-container">
+      <header>header</header>
+      <div className="content-container" />
+      <footer>footer</footer>
+    </div>
+  );
+};
+
+export default Layout;
