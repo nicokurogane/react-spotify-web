b47ca4411d25730942b83849abd9f821157a789c
nicokurogane <fyedream@gmail.com>
2019-08-24 18:44:49 -0600

    Implemented Search Debouncing

--- src/components/albums/search-bar/SearchBar.js
+++ src/components/albums/search-bar/SearchBar.js
@@ -6,17 +6,36 @@ import './search-bar.scss';
 const { Search } = Input;
 
 class SearchBar extends React.Component {
+  state = {
+    searchTerm: ''
+  };
+
   render() {
     return (
       <div className="search-bar-container">
         <Search
           placeholder="Busca un album"
-          onSearch={value => console.log(value)}
-          enterButton
+          onChange={e => this.setSearchTerm(e.target.value)}
         />
       </div>
     );
   }
+
+  debounce = (fn, time) => {
+    let timeout;
+
+    return function() {
+      const functionCall = () => fn.apply(this, arguments);
+
+      clearTimeout(timeout);
+      timeout = setTimeout(functionCall, time);
+    };
+  };
+
+  setSearchTerm = this.debounce(searchTerm => {
+    console.log(searchTerm);
+    this.setState({ searchTerm });
+  }, 1000);
 }
 
 export default SearchBar;
--- src/data/network-utils/spotify/albums/index.js
+++ src/data/network-utils/spotify/albums/index.js
@@ -4,7 +4,6 @@ export const getAlbums = async () => {
   let serverResponse = await spotifyClient.get(
     '/albums?ids=41MnTivkwTO3UUJ8DrqEJJ,6JWc4iAiJ9FjyK0B59ABb4,6UXCm6bOO4gFlDQZV5yL37'
   );
-  //  .then(response => response);
   return serverResponse;
 };
 
