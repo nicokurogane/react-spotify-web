ae71cb80bf39610661e733c59244df9c13617717
nicokurogane <fyedream@gmail.com>
2019-08-25 18:36:18 -0600

    fixed millisecond and footer

--- src/components/layout-app/LayoutApp.js
+++ src/components/layout-app/LayoutApp.js
@@ -19,7 +19,35 @@ const LayoutApp = ({ children }) => {
           <ProfileAvatar />
         </Header>
         <Content className="content">{children}</Content>
-        <Footer className="app-footer">Footer</Footer>
+        <Footer className="app-footer">
+          <div>
+            <img src={spotifyLogo} className="footer-logo" alt="header logo" />
+          </div>
+          <div className="main-info">
+            <div className="info">
+              <span className="info-title">Company</span>
+              <span>About</span>
+              <span>Employee</span>
+            </div>
+            <div className="info">
+              <span className="info-title">Comunities</span>
+              <span>For artists</span>
+              <span>Developers</span>
+            </div>
+            <div className="info">
+              <span className="info-title">Useful Links</span>
+              <span>Help</span>
+              <span>Web Player</span>
+            </div>
+          </div>
+          <div className="additional-link-container">
+            <span className="addition-link">Legal</span>
+            <span className="addition-link">Privacy Center</span>
+            <span className="addition-link">Privacy Politic</span>
+            <span className="addition-link">Cookies</span>
+            <span className="addition-link">Abouts Ads</span>
+          </div>
+        </Footer>
       </Layout>
     </div>
   );
--- src/components/tracks/item/Item.js
+++ src/components/tracks/item/Item.js
@@ -17,7 +17,14 @@ const millisecondsToMinutesAndSeconds = timeInMilliseconds => {
   if (!timeInMilliseconds) return '';
   const seconds = 1000 * Math.ceil(timeInMilliseconds / 1000); // round to nearest second
   const date = new Date(seconds);
-  return `${date.getUTCMinutes()}:${date.getUTCSeconds()}`; // "4:59"
+  return `${date.getUTCMinutes()}:${addZero(date.getUTCSeconds())}`; // "4:59"
+};
+
+const addZero = second => {
+  if (second < 10) {
+    second = `0${second}`;
+  }
+  return second;
 };
 
 export default Item;
